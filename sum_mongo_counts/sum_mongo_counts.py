f7273fcdda2ba1d838ba1b283bce24c945927579
eric <gorskie@moravian.edu>
2022-02-11 14:30:58 -0500

    created function shells and documentation for mongo db counter

---
+++ sum_mongo_counts/sum_mongo_counts.py
@@ -0,0 +1,12 @@
+"""
+This module contains the functions that will be used to query the mongoDB    
+"""
+
+def get_count_one_db():
+    """ returns the number of entries in a mongo table as an int """
+    pass
+
+
+def get_count_all_tables():
+    """ returns the number of entries in all tables in a mongo db as an int """
+    pass
