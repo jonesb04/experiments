dcfa0d4ffedb2bed498d0194e4b82b5d6fbfa044
fineanganofoj <fineanganofoj@moravian.edu>
2022-02-18 11:10:32 -0500

    Sends content instead of url to flask

--- client.py
+++ client.py
@@ -2,6 +2,9 @@
 Allows clients to save 
 
 Comments with attachments are not saved, with this (c_Attachemnt) we can finally get what the real attachments are
+
+Does the URL have to be the actual pdf or a link to a comment, scraping the pdf?
+- No just take attachment URL's
 '''
 
 import requests
@@ -13,16 +16,19 @@ load_dotenv()
 
 """
 Client receives url to the attachment and url to the server
+
+TODO: Client sends data, not attachment
+TODO: Client does not save to disk, but saves the data? hmm
 """
 class Client():
 
     def __init__(self, attachment_url, server_url):
         self.attachment_url = attachment_url
         self.server_url = server_url
-        self.attach_requests = requests.get(self.attachment_url)
+        attach_requests = requests.get(self.attachment_url)
         # Needs to check if this works for other files, future testing note
-        self.file_name = self.attach_requests.headers['Content-Disposition'].split('"')[1]
-        self.content = self.attach_requests.content
+        self.file_name = attach_requests.headers['Content-Disposition'].split('"')[1]
+        self.content = attach_requests.content
 
     # This function saves the pdf to disk from the url
     def save_to_disk(self):
@@ -32,7 +38,7 @@ class Client():
     # This function send the attachment url to the server
     def send_data(self):
         # Have to make sure encodings are always bytes represented, future testing note
-        requests.post(self.server_url + "/attachment", data=str.encode(self.attachment_url))
+        requests.post(self.server_url + "/attachment", data=self.content)
 
 # Run these two lines to test it out
 client_test = Client('https://downloads.regulations.gov/EPA-HQ-OW-2021-0602-0130/attachment_1.pdf', "http://127.0.0.1:5000")
